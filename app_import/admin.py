2fa5b15cda81e0407679413a8e66e07c98ed88a0
Mateus <147616326+mbevaldt@users.noreply.github.com>
2023-12-21 06:58:43 -0300

    Delete app_import directory

--- app_import/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Invoice, ProForma, Ledger, BillLading
-
-admin.site.register(Invoice)
-admin.site.register(ProForma)
-admin.site.register(Ledger)
-admin.site.register(BillLading)
--- app_import/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppImportConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app_import"
